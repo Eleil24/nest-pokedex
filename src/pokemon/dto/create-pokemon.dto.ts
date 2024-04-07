ce585e42a68182c61b335ba518e3ad07df2f36fc
Eleil24 <eleil65723@gmail.com>
2024-04-07 12:07:21 -0500

    Fin seccion 7

---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,16 @@
+import { IsInt, IsPositive, IsString, Min, MinLength } from "class-validator";
+
+
+
+export class CreatePokemonDto {
+    //isInt, isPositive, min 1
+    @IsInt()
+    @IsPositive()
+    @Min(1)
+    no: number;
+
+    @IsString()
+    @MinLength(1)
+    //isString, minlenth 1
+    name: string;
+}
